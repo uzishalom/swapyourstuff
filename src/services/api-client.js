37a93f06c60a98a90baadb2d9ff1e854af3635e7
uzishalom <64908639+uzishalom@users.noreply.github.com>
2020-08-01 02:50:11 +0300

    removing not needed axios definition

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
-axios.defaults.headers.delete = { "Content-Type": "application/json;charset=utf-8" };
 
 export default {
     get: axios.get,
